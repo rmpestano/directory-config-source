4bd1b044b0385f5c1521cfb2d43ade935747a8ba
rafael-pestano <rafael-pestano@procergs.rs.gov.br>
2019-02-27 16:59:34 -0300

    perfumery

--- src/test/java/com/github/rmpestano/config/PropertyBean.java
+++ src/test/java/com/github/rmpestano/config/PropertyBean.java
@@ -11,10 +11,10 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
 
-
 /**
- * This bean is needed because there is no standard way to pass system properties to arquillian containers
- * And our DirectoryConfigSource needs a system property which has the path to the config file
+ * This bean is needed because there is no standard way to pass system
+ * properties to arquillian containers And our DirectoryConfigSource needs a
+ * system property which has the path to the config file
  * 
  * @author rafael-pestano
  *
@@ -22,21 +22,24 @@ import javax.inject.Inject;
 @Startup
 @Singleton
 public class PropertyBean implements Serializable {
-	
+
 	@Inject
 	DirectoryConfigSource directoryConfigSource;
 
-	@PostConstruct
-	public void initDirectoryConfigSourceProperty() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, IOException {
-		Field configPropertiesMapField = directoryConfigSource.getClass().getDeclaredField("configPropertiesMap");
-		configPropertiesMapField.setAccessible(true);
-		configPropertiesMapField.set(directoryConfigSource, null); //force re-initialization of configMap because it is initialized before test execution
-		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-				Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/file")))) {
-			String property = bufferedReader.readLine();
-			System.out.println("PROPERTY FROM FILE:"+property);
-			System.setProperty("DirectoryConfigSource.filePath", property);
+	@PostConstruct 
+	public void initDirectoryConfigSourceProperty() {
+		try {
+			Field configPropertiesMapField = directoryConfigSource.getClass().getDeclaredField("configPropertiesMap");
+			configPropertiesMapField.setAccessible(true);
+			configPropertiesMapField.set(directoryConfigSource, null); // force re-initialization of configMap because the test runs after Config source initialization (before bean discovery)
+			try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+					getClass().getResourceAsStream("/META-INF/file")))) {
+				String property = bufferedReader.readLine();
+				System.setProperty("DirectoryConfigSource.filePath", property);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
 	}
 }
